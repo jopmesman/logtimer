fc10295fe51010635b7abf8c33047c35a6cd27fd
jopmesman <jopmesman@hotmail.com>
2019-08-26 20:16:31 +0200

    Simple class to log timers

---
+++ src/LogTimer.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Jopmesman\TimeLogger;
+
+/**
+ * Class LogTimer
+ * @package Jopmesman\TimeLogger
+ */
+class LogTimer
+{
+    public static $timers = [];
+
+    public static $times = [];
+
+    /**
+     * @param string $key
+     */
+    public static function time(string $key)
+    {
+        if (!array_key_exists($key, self::$timers)) {
+            self::$timers[$key] = microtime(true);
+        } else {
+            $end = microtime(true);
+            self::$times[$key] =  $end - self::$timers[$key];
+            unset(self::$timers[$key]);
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public static function times()
+    {
+        return self::$times;
+    }
+}
